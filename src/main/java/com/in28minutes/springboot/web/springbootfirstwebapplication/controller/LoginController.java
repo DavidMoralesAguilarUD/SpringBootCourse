5ed07f1e21b4703e6966e65c5b0f3fe8a6f62052
DAvid <davidtrokten@gmail.com>
2020-04-22 19:26:29 -0500

    @SessionAttributes

--- src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -6,14 +6,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.springbootfirstwebapplication.service.LoginService;
 
 @Controller
+@SessionAttributes("name")
 public class LoginController {
 	
 	@Autowired
-	LoginService service = new LoginService();
+	LoginService service;
 	 
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
--- src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
+++ src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.service.TodoService;
-import com.in28minutes.springboot.web.springbootfirstwebapplication.service.LoginService;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 	
 	@Autowired
@@ -18,6 +18,7 @@ public class TodoController {
 	 
 	@RequestMapping(value="/list-todos", method = RequestMethod.GET)
 	public String showTodosList(ModelMap model) {
+		String name = (String) model.get("name");
 		model.put("todos", service.retrieveTodos("in28Minutes"));
 		return "list-todos";		
 	}
